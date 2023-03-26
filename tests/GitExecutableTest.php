1dcfe2f5c239e0577c69da6c36a8f3adbe4e9b89
Martin Auswöger <martin@auswoeger.com>
2023-03-26 16:24:50 +0200

    Coding style

--- tests/GitExecutableTest.php
+++ tests/GitExecutableTest.php
@@ -1,5 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the ausi/remote-git package.
+ *
+ * (c) Martin Auswöger <martin@auswoeger.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ausi\RemoteGit\Tests;
 
 use Ausi\RemoteGit\Exception\ExecutableNotFoundException;
